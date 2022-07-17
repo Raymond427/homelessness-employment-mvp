81363e58b29903dd2782d5e19234a2947401b2bc
Raymond427 <rferrel427@gmail.com>
2022-07-16 22:12:14 -0700

    patch customer id field in payment intent

--- functions/index.js
+++ functions/index.js
@@ -1,67 +1,84 @@
-const functions = require('firebase-functions')
-const admin = require('firebase-admin')
-const Stripe = require('stripe')
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+const Stripe = require("stripe");
 
-admin.initializeApp()
-const db = admin.firestore()
+admin.initializeApp();
+const db = admin.firestore();
 
-const stripe = new Stripe(functions.config().stripe.secret)
+const stripe = new Stripe(functions.config().stripe.secret);
 
-exports.createStripeCustomer = functions.auth.user().onCreate(
-    async ({ uid, email, displayName, metadata: { creationTime } }) => {
-        const firebaseuid = uid
-        const customer = email ? await stripe.customers.create({ email, name: displayName, metadata: { firebaseuid } }) : { id: null }
+exports.createStripeCustomer = functions.auth
+  .user()
+  .onCreate(async ({ uid, email, displayName, metadata: { creationTime } }) => {
+    const firebaseuid = uid;
+    const customer = email
+      ? await stripe.customers.create({
+          email,
+          name: displayName,
+          metadata: { firebaseuid }
+        })
+      : { id: null };
 
-        return db.doc(`users/${firebaseuid}`).create({ email, creationTime, stripeid: customer.id })
-    }
-)
+    return db
+      .doc(`users/${firebaseuid}`)
+      .create({ email, creationTime, stripeid: customer.id });
+  });
 
 exports.createPaymentIntent = functions.https.onCall(
-    async (paymentIntentArgs, context) => {
-        const firebaseuid = context.auth.uid
-        const user = await db.doc(`users/${firebaseuid}`).get()
-        const userStripeCustomerId = user.data() ? user.data().stripeid : null
-        
-        try {
-            return await stripe.paymentIntents.create(user && user.email && userStripeCustomerId ? { ...paymentIntentArgs, customer: userStripeCustomerId } : paymentIntentArgs)
-        } catch (error) {
-            return error
-        }
+  async paymentIntentArgs => {
+    const firebaseuid = paymentIntentArgs.metadata.firebase_uid;
+    const userStripeCustomerId = paymentIntentArgs.metadata.guest_user
+      ? null
+      : await (await db.doc(`users/${firebaseuid}`).get()).data().stripeid;
+
+    try {
+      return await stripe.paymentIntents.create({
+        ...paymentIntentArgs,
+        customer: userStripeCustomerId
+      });
+    } catch (error) {
+      return error;
     }
-)
+  }
+);
 
 exports.onPaymentSuccessful = functions.https.onRequest(
-    async (request, response) => {
-        const requestSignature = request.headers['stripe-signature']
-        const signingKey = functions.config().stripe.signing_key
+  async (request, response) => {
+    const requestSignature = request.headers["stripe-signature"];
+    const signingKey = functions.config().stripe.signing_key;
 
-        try {
-            stripe.webhooks.constructEvent(request.rawBody, requestSignature, signingKey)
-        } catch (err) {
-            return response.status(400).end()
-        }
-        
-        const donationObject = request.body.data.object
-        const { id, amount, created, currency, metadata } = donationObject
-        const donation = {
-            stripeId: id,
-            ...metadata,
-            donation_amount: Number(metadata.donation_amount),
-            processing_fee: Number(metadata.processing_fee),
-            anonymous_donation: metadata.anonymous_donation === 'true',
-            totalAmount: amount,
-            created: Number(created) * 1000,
-            currency
-        }
-
-        db.collection('/donations').add(donation)
-        const campaignRef = db.collection(`/campaign`).doc(metadata.campaign_id)
-        const campaignData = (await campaignRef.get()).data()
-        campaignRef.update({
-            amountDonated: campaignData.amountDonated + Number(metadata.donation_amount),
-            donationCount: campaignData.donationCount + 1,
-        })
-
-        return response.status(200).end()
+    try {
+      stripe.webhooks.constructEvent(
+        request.rawBody,
+        requestSignature,
+        signingKey
+      );
+    } catch (err) {
+      return response.status(400).end();
     }
-)
+
+    const donationObject = request.body.data.object;
+    const { id, amount, created, currency, metadata } = donationObject;
+    const donation = {
+      stripeId: id,
+      ...metadata,
+      donation_amount: Number(metadata.donation_amount),
+      processing_fee: Number(metadata.processing_fee),
+      anonymous_donation: metadata.anonymous_donation === "true",
+      totalAmount: amount,
+      created: Number(created) * 1000,
+      currency
+    };
+
+    db.collection("/donations").add(donation);
+    const campaignRef = db.collection(`/campaign`).doc(metadata.campaign_id);
+    const campaignData = (await campaignRef.get()).data();
+    campaignRef.update({
+      amountDonated:
+        campaignData.amountDonated + Number(metadata.donation_amount),
+      donationCount: campaignData.donationCount + 1
+    });
+
+    return response.status(200).end();
+  }
+);
--- src/utils/payments.js
+++ src/utils/payments.js
@@ -1,91 +1,141 @@
-import firebase from '../firebase'
-import { capitalize } from '.'
-import { performanceMonitor, analytics } from '../firebase'
+import firebase from "../firebase";
+import { capitalize } from ".";
+import { performanceMonitor, analytics } from "../firebase";
 
-const MARGIN = 0.10
+const MARGIN = 0.1;
 
-const stripeProcessingFee = amount => (amount * 0.029) + 30
+const stripeProcessingFee = amount => amount * 0.029 + 30;
 
-export const calculateProcessingFee = amount => stripeProcessingFee(amount) + (amount * MARGIN)
+export const calculateProcessingFee = amount =>
+  stripeProcessingFee(amount) + amount * MARGIN;
 
-export const paymentIntentArgsFactory = (donee, totalCost, source, user, donationAmount, donationMessage, processingFee, nameOnCard, anonymousDonation) => ({
-    amount: totalCost,
-    currency: 'usd',
-    description: `Donation to ${capitalize(donee.firstName)} ${capitalize(donee.lastName)}`,
-    metadata: {
-        campaign_name: `${capitalize(donee.firstName)} ${capitalize(donee.lastName)}`,
-        donation_amount: donationAmount,
-        processing_fee: processingFee,
-        firebase_uid: user.uid,
-        donationMessage,
-        campaign_id: donee.id,
-        anonymous_donation: anonymousDonation,
-        photoURL: user.photoURL,
-        name_on_card: nameOnCard
-    },
-    payment_method: source,
-    payment_method_types: [ 'card' ],
-    receipt_email: user.email
-})
+export const paymentIntentArgsFactory = (
+  donee,
+  totalCost,
+  source,
+  user,
+  donationAmount,
+  donationMessage,
+  processingFee,
+  nameOnCard,
+  anonymousDonation
+) => ({
+  amount: totalCost,
+  currency: "usd",
+  description: `Donation to ${capitalize(donee.firstName)} ${capitalize(
+    donee.lastName
+  )}`,
+  metadata: {
+    campaign_name: `${capitalize(donee.firstName)} ${capitalize(
+      donee.lastName
+    )}`,
+    donation_amount: donationAmount,
+    processing_fee: processingFee,
+    firebase_uid: user.uid,
+    guest_user: user.isAnonymous,
+    donationMessage,
+    campaign_id: donee.id,
+    anonymous_donation: anonymousDonation,
+    photoURL: user.photoURL,
+    name_on_card: nameOnCard
+  },
+  payment_method: source,
+  payment_method_types: ["card"],
+  receipt_email: user.email
+});
 
-export const chargeCard = async (confirmCardPayment, source, { donee, anonymousDonation, donationAmount, donationMessage, user, totalCost, processingFee, method }, nameOnCard) => {
-    const chargeTrace = performanceMonitor.trace('charge')
-    chargeTrace.start()
-    chargeTrace.putAttribute('method', method)
+export const chargeCard = async (
+  confirmCardPayment,
+  source,
+  {
+    donee,
+    anonymousDonation,
+    donationAmount,
+    donationMessage,
+    user,
+    totalCost,
+    processingFee,
+    method
+  },
+  nameOnCard
+) => {
+  const chargeTrace = performanceMonitor.trace("charge");
+  chargeTrace.start();
+  chargeTrace.putAttribute("method", method);
 
-    try {
-        const paymentIntentArgs = paymentIntentArgsFactory(donee, totalCost, source, user, donationAmount, donationMessage, processingFee, nameOnCard, anonymousDonation)
-        const createPaymentIntent = firebase.functions().httpsCallable('createPaymentIntent')
+  try {
+    const paymentIntentArgs = paymentIntentArgsFactory(
+      donee,
+      totalCost,
+      source,
+      user,
+      donationAmount,
+      donationMessage,
+      processingFee,
+      nameOnCard,
+      anonymousDonation
+    );
+    const createPaymentIntent = firebase
+      .functions()
+      .httpsCallable("createPaymentIntent");
 
-        const { data: paymentIntent } = await createPaymentIntent(paymentIntentArgs)
+    const { data: paymentIntent } = await createPaymentIntent(
+      paymentIntentArgs
+    );
 
-        analytics.logEvent('purchase', {
-            currency: paymentIntent.currency,
-            items: [
-                {
-                    id: donee.id,
-                    name: `${capitalize(donee.firstName)} ${capitalize(donee.lastName)}`,
-                    quantity: 1,
-                    price: donationAmount / 100
-                }
-            ],
-            transaction_id: paymentIntent.id,
-            tax: processingFee / 100,
-            value: donationAmount / 100
-        })
+    analytics.logEvent("purchase", {
+      currency: paymentIntent.currency,
+      items: [
+        {
+          id: donee.id,
+          name: `${capitalize(donee.firstName)} ${capitalize(donee.lastName)}`,
+          quantity: 1,
+          price: donationAmount / 100
+        }
+      ],
+      transaction_id: paymentIntent.id,
+      tax: processingFee / 100,
+      value: donationAmount / 100
+    });
 
-        chargeTrace.putAttribute('result', 'success')    
-        chargeTrace.putAttribute('status', paymentIntent.status)
+    chargeTrace.putAttribute("result", "success");
+    chargeTrace.putAttribute("status", paymentIntent.status);
 
-        const confirmCardPaymentResult = await confirmCardPayment(paymentIntent.client_secret, {
-            payment_method: source,
-            receipt_email: user.email
-        })
+    const confirmCardPaymentResult = await confirmCardPayment(
+      paymentIntent.client_secret,
+      {
+        payment_method: source,
+        receipt_email: user.email
+      }
+    );
 
-        chargeTrace.stop()
+    chargeTrace.stop();
 
-        return confirmCardPaymentResult
-    } catch (error) {
-        chargeTrace.putAttribute('result', 'fail')
-        chargeTrace.stop()
-        return error
-    }
-}
+    return confirmCardPaymentResult;
+  } catch (error) {
+    chargeTrace.putAttribute("result", "fail");
+    chargeTrace.stop();
+    return error;
+  }
+};
 
-export const isPaymentError = error => (
-    (error instanceof Error) || (error.stack && error.message) || error.error || (typeof error.status === 'string' && error.status.includes('Error'))
-)
+export const isPaymentError = error =>
+  error instanceof Error ||
+  (error.stack && error.message) ||
+  error.error ||
+  (typeof error.status === "string" && error.status.includes("Error"));
 
 export const handlePaymentError = (setPaymentResult, error) => {
-    const genericErrorMessage = "We're having some trouble connecting to the internet, check that your connection is good then try again"
+  const genericErrorMessage =
+    "We're having some trouble connecting to the internet, check that your connection is good then try again";
 
-    if (error.error) {
-        setPaymentResult(error.error.message || genericErrorMessage)
-    } else if (error.raw) {
-        setPaymentResult(error.raw.message || genericErrorMessage)
-    } else if (error.type === 'card_error') {
-        setPaymentResult(error.message)
-    } else {
-        setPaymentResult(genericErrorMessage)
-    }
-}
+  if (error.error) {
+    setPaymentResult(error.error.message || genericErrorMessage);
+  } else if (error.raw) {
+    setPaymentResult(error.raw.message || genericErrorMessage);
+  } else if (error.type === "card_error") {
+    setPaymentResult(error.message);
+  } else {
+    setPaymentResult(genericErrorMessage);
+  }
+};
